db17b41294dc8f8cf2631f8f63b17d04e6adf21a
Nikita Bogachev <nikita.bogachyov@noorsoft.ru>
2021-06-09 13:44:56 +0500

    fixed DB-56

--- src/components/DialogIsOver/DialogIsOver.jsx
+++ src/components/DialogIsOver/DialogIsOver.jsx
@@ -3,14 +3,14 @@ import moment from 'moment';
 import classes from './DialogIsOver.module.css'
 
 export default function DialogIsOver({ timestamp, chatRate }) {
+  
+  const fullOrEmptyStar = (i, rate) => <i key={`star_${i}`} className={`${i < rate ? 'fas' : 'far'} fa-star gold-star`}></i>
+
   const rate = () =>
     Array(5)
       .fill('')
-      .map((_, i) => {
-        if (i < chatRate) {
-          return <i key={`star_${i}`} className="fas fa-star gold-star"></i>;
-        } else return <i key={`star_${i}`} className="far fa-star gold-star"></i>;
-      });
+      .map((_, i) => fullOrEmptyStar(i, chatRate));
+
   return (
     <div className={classes.DialogIsOverWrapper}>
       <i>Диалог завершился {moment(timestamp).fromNow()}</i>
